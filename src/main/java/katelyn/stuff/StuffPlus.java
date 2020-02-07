e31bcfdb818564b1b6a2b92db14e449021d319e2
ArtsyTornado <60759451+ArtsyTornado@users.noreply.github.com>
2020-02-07 06:25:55 -0600

    Update StuffPlus.java

--- src/main/java/katelyn/stuff/StuffPlus.java
+++ src/main/java/katelyn/stuff/StuffPlus.java
@@ -12,28 +12,28 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod("stuffplus")
 public class StuffPlus 
 {
-	public static StuffPlus instance;
-	public static final String modid = "stuffplus";
-	private static final Logger logger = LogManager.getLogger(modid);
+    public static StuffPlus instance;
+    public static final String modid = "stuffplus";
+    private static final Logger logger = LogManager.getLogger(modid);
 	
-	public StuffPlus() 
-	{
-		instance = this;
+public StuffPlus() 
+{
+	instance = this;
 		
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
+	FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+	FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
 		
-		MinecraftForge.EVENT_BUS.register(this);
+	MinecraftForge.EVENT_BUS.register(this);
 
-	}
+}
 	
-	private void setup(final FMLCommonSetupEvent event)
-	{
-		logger.info("setup complete");
-	}
+    private void setup(final FMLCommonSetupEvent event)
+    {
+	logger.info("setup complete");
+    }
 
-	private void clientRegistries(final FMLClientSetupEvent event)
-	{
-		logger.info("clientRegistries loading complete");
-	}
+    private void clientRegistries(final FMLClientSetupEvent event)
+    {
+	logger.info("clientRegistries loading complete");
+    }
 }
